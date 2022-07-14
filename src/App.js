46e56ff6b8e90e42b8839928d7ffddc6ae806b8b
jung89 <104253822+junghyun89@users.noreply.github.com>
2022-07-14 21:16:36 +0900

    ing

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './components/navbar';
+import HeroSection from './components/HeroSection';
+import ImproveSkills from './components/ImproveSkills';
+import QuoteSection from './components/QuoteSection';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container main">
+        <HeroSection />
+        <ImproveSkills />
+        <QuoteSection />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuoteSection.js
@@ -0,0 +1,12 @@
+export default function QuoteSection() {
+  return (
+    <div className="section quote">
+      <p className="quote-text">
+        Food is everything we are. It's an extension of nationalist feeling,
+        ethnic feeling, your personal history, your province, your region, your
+        tribe, your grandma. It's inseparable from those from the get-go.
+      </p>
+      <p className="qoute-auther">- Anthony Bourdain</p>
+    </div>
+  );
+}
